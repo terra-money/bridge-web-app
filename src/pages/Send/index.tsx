7fd518d67f653faebf61b944f11c59da14ade620
blue <blue@terra.money>
2021-03-17 23:06:31 +0900

    Revert "notice site Under Construction"
    This reverts commit 47e49898583afe49eaa60bc5710fed0488754965.

--- src/pages/Send/index.tsx
+++ src/pages/Send/index.tsx
@@ -1,30 +1,28 @@
 import { ReactElement } from 'react'
-// import { useSetRecoilState } from 'recoil'
+import { useSetRecoilState } from 'recoil'
 
-// import { useModal } from 'components/Modal'
-// import SendProcessModal from 'components/Modal/SendProcessModal'
+import { useModal } from 'components/Modal'
+import SendProcessModal from 'components/Modal/SendProcessModal'
 
-// import SendProcessStore, { ProcessStatus } from 'store/SendProcessStore'
+import SendProcessStore, { ProcessStatus } from 'store/SendProcessStore'
 
-// import SendForm from './SendForm'
-import UnderConstruction from './UnderConstruction'
+import SendForm from './SendForm'
 
 const Send = (): ReactElement => {
-  // const sendTxModal = useModal()
-  // const setSendProcessStatus = useSetRecoilState(
-  //   SendProcessStore.sendProcessStatus
-  // )
+  const sendTxModal = useModal()
+  const setSendProcessStatus = useSetRecoilState(
+    SendProcessStore.sendProcessStatus
+  )
 
-  // const onClickSendButton = async (): Promise<void> => {
-  //   setSendProcessStatus(ProcessStatus.Confirm)
-  //   sendTxModal.open()
-  // }
+  const onClickSendButton = async (): Promise<void> => {
+    setSendProcessStatus(ProcessStatus.Confirm)
+    sendTxModal.open()
+  }
 
   return (
     <>
-      <UnderConstruction />
-      {/* <SendForm onClickSendButton={onClickSendButton} />
-      <SendProcessModal {...sendTxModal} /> */}
+      <SendForm onClickSendButton={onClickSendButton} />
+      <SendProcessModal {...sendTxModal} />
     </>
   )
 }
