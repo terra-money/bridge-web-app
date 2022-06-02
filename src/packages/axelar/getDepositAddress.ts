a210c63e2339cc14697384533d99e57d2de0434a
alecande11 <54709706+alecande11@users.noreply.github.com>
2022-06-02 16:25:01 +0200

    calculate Axelar fee

--- src/packages/axelar/getDepositAddress.ts
+++ src/packages/axelar/getDepositAddress.ts
@@ -7,6 +7,15 @@ const sdk = new AxelarAssetTransfer({
   auth: 'local',
 })
 
+const tokens: Record<string, string> = {
+  'ibc/B3504E092456BA618CC28AC671A71FB08C6CA0FD0BE7C8A5B5A3E2DD933CC9E4':
+    'uusdc',
+  'ibc/CBF67A2BCF6CAE343FDF251E510C8E18C361FC02B23430C121116E0811835DEF':
+    'uusdt',
+  'ibc/05D299885B07905B6886F554B39346EA6761246076A1120B1950049B92B922DD':
+    'wbtc-satoshi',
+}
+
 export async function getDepositAddress(
   destinationAddress: string,
   fromBlockChain: BlockChainType,
@@ -17,7 +26,7 @@ export async function getDepositAddress(
     fromBlockChain,
     toBlockChain,
     destinationAddress,
-    coin
+    tokens[coin]
   )
 }
 
@@ -26,5 +35,10 @@ export async function getAxelarFee(
   toBlockChain: BlockChainType,
   coin: string
 ): Promise<number> {
-  return 1
+  if (tokens[coin] === 'uusdt' || tokens[coin] === 'uusdc') {
+    return 60.5 * (fromBlockChain === BlockChainType.terra ? 1e6 : 1e18)
+  } else {
+    // WBTC
+    return 0.00072 * 1e8
+  }
 }
