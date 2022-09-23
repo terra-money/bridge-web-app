a444f98b200800da0f2ad28792c3f22b25b07c58
alecande11 <54709706+alecande11@users.noreply.github.com>
2022-09-23 08:52:01 +0200

    Revert "disable bridge"
    This reverts commit 9affaf15b26421af92b14befd639e77acd4524e8.

--- src/pages/Send/SendFormButton/NextOrApproveButton.tsx
+++ src/pages/Send/SendFormButton/NextOrApproveButton.tsx
@@ -5,6 +5,7 @@ import { CircularProgress } from '@material-ui/core'
 
 import { COLOR } from 'consts'
 
+import { BlockChainType, BridgeType } from 'types/network'
 import { RequestTxResultType, ValidateItemResultType } from 'types/send'
 import useSend from 'hooks/useSend'
 
@@ -21,11 +22,19 @@ const NextOrApproveButton = ({
 }): ReactElement => {
   const setStatus = useSetRecoilState(SendProcessStore.sendProcessStatus)
 
+  const fromBlockChain = useRecoilValue(SendStore.fromBlockChain)
+  const bridgeUsed = useRecoilValue(SendStore.bridgeUsed)
+  const validationResult = useRecoilValue(SendStore.validationResult)
   const amount = useRecoilValue(SendStore.amount)
 
   const [waitingForApprove, setWaitingForApprove] = useState(false)
   const [approveResult, setApproveResult] = useState<RequestTxResultType>()
   const { allowanceOfSelectedAsset, approveTxFromEtherBase } = useSend()
+
+  const ableButton =
+    fromBlockChain === BlockChainType.terra
+      ? validationResult.isValid && feeValidationResult.isValid
+      : validationResult.isValid
 
   const onClickApproveTxFromEtherBase = async (): Promise<void> => {
     setWaitingForApprove(true)
@@ -68,8 +77,22 @@ const NextOrApproveButton = ({
   }
 
   return (
-    <Button onClick={onClickSendNextButton} disabled={true}>
-      Not available
+    <Button
+      onClick={onClickSendNextButton}
+      disabled={
+        !ableButton ||
+        bridgeUsed === BridgeType.ibc ||
+        bridgeUsed === BridgeType.wormhole ||
+        (bridgeUsed === BridgeType.shuttle &&
+          fromBlockChain === BlockChainType.terra)
+      }
+    >
+      {bridgeUsed === BridgeType.ibc ||
+      bridgeUsed === BridgeType.wormhole ||
+      (bridgeUsed === BridgeType.shuttle &&
+        fromBlockChain === BlockChainType.terra)
+        ? 'Not available'
+        : 'Next'}
     </Button>
   )
 }
