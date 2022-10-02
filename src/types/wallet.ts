f532d0266453eaac401a55f769e1fa42b26c5917
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2022-10-02 17:09:43 +0200

    Enable Keplr on firefox

--- src/types/wallet.ts
+++ src/types/wallet.ts
@@ -53,7 +53,7 @@ export const WalletSupportBrowser: Record<
     errorMessage: 'Available for desktop browsers.',
   },
   Keplr: {
-    isSupport: isBrowser && (isChrome || isEdgeChromium),
-    errorMessage: 'Available for desktop Chrome.',
+    isSupport: isBrowser && (isChrome || isEdgeChromium || isFirefox),
+    errorMessage: 'Available for desktop Chrome and Firefox.',
   },
 }
