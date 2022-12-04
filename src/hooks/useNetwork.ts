9f30e148259142534bb6c7821ef1cc093b31a379
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2022-12-04 13:58:24 +0100

    Fix Kujira explorer

--- src/hooks/useNetwork.ts
+++ src/hooks/useNetwork.ts
@@ -37,28 +37,32 @@ const useNetwork = (): {
       return `https://terrasco.pe/${
         isTestnet ? 'testnet' : 'mainnet'
       }/${type}/${address}`
+    } else if (fromBlockChain === BlockChainType.kujira) {
+      return type === 'tx'
+        ? `https://finder.kujira.app/kaiyo-1/tx/${address}`
+        : `https://finder.kujira.app/kaiyo-1/address/${address}`
+    } else if (fromBlockChain === BlockChainType.osmo) {
+      return type === 'tx'
+        ? `https://www.mintscan.io/osmosis/txs/${address}`
+        : `https://www.mintscan.io/osmosis/account/${address}`
+    } else if (fromBlockChain === BlockChainType.scrt) {
+      return type === 'tx'
+        ? `https://www.mintscan.io/secret/txs/${address}`
+        : `https://www.mintscan.io/secret/account/${address}`
+    } else if (fromBlockChain === BlockChainType.inj) {
+      return type === 'tx'
+        ? `https://www.mintscan.io/injective/txs/${address}`
+        : `https://www.mintscan.io/injective/account/${address}`
+    } else if (fromBlockChain === BlockChainType.cosmos) {
+      return type === 'tx'
+        ? `https://www.mintscan.io/cosmos/txs/${address}`
+        : `https://www.mintscan.io/cosmos/account/${address}`
     } else if (etherBaseExt) {
       let subdomain = ''
 
       if (fromBlockChain === BlockChainType.bsc) {
         subdomain = isTestnet ? 'testnet.' : ''
         return `https://${subdomain}bscscan.com/${type}/${address}`
-      } else if (fromBlockChain === BlockChainType.osmo) {
-        return type === 'tx'
-          ? `https://www.mintscan.io/osmosis/txs/${address}`
-          : `https://www.mintscan.io/osmosis/account/${address}`
-      } else if (fromBlockChain === BlockChainType.scrt) {
-        return type === 'tx'
-          ? `https://www.mintscan.io/secret/txs/${address}`
-          : `https://www.mintscan.io/secret/account/${address}`
-      } else if (fromBlockChain === BlockChainType.inj) {
-        return type === 'tx'
-          ? `https://www.mintscan.io/injective/txs/${address}`
-          : `https://www.mintscan.io/injective/account/${address}`
-      } else if (fromBlockChain === BlockChainType.cosmos) {
-        return type === 'tx'
-          ? `https://www.mintscan.io/cosmos/txs/${address}`
-          : `https://www.mintscan.io/cosmos/account/${address}`
       } else if (fromBlockChain === BlockChainType.polygon) {
         return `https://polygonscan.com/${type}/${address}`
       } else if (fromBlockChain === BlockChainType.avalanche) {
